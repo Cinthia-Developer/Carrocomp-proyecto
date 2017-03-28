3e0f8b1ce64e29fbd4942dbe65f5406ef07ba83f
Cinthia <cinthiamashiel474@gmail.com>
2017-03-28 14:37:37 -0500

    Se agregó mapa.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,21 @@ var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-minify-css');
 
+//---------- desde aqui-----
+var bootstrap = require("bootstrap");
+    
+gulp.task('js', function(){
+  return gulp.src('node_modules/bootstrap/dist/js/bootstrap.min.js')
+    .pipe(bootstrap())
+    .pipe(gulp.dest('src/js'))
+});
+
+gulp.task('default', ['js']);
+
+
+//------- hasta aqui ----
+
+
 
 gulp.task('script', function(cb){
 	// tarea script
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
-	// tu codigo va acá
-});
+//---- Función Init ----
+function init(){
+    initMap();
+}
+//---- Función del mapa ----
+function initMap() {
+    var map;
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -33.43784391048617, lng: -70.65047959999998},
+        zoom: 10
+  });
+}
