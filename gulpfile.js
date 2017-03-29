8bb4f9990c86192c3f4487723c66f2a2b75ce126
Cinthia <cinthiamashiel474@gmail.com>
2017-03-29 09:36:04 -0500

    Se agregaron las dependencias mediante gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,28 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
+/*var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var notify = require('gulp-notify');
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-minify-css');
-
+*/
 //---------- desde aqui-----
-var bootstrap = require("bootstrap");
     
-gulp.task('js', function(){
+gulp.task('javascript', function(){
   return gulp.src('node_modules/bootstrap/dist/js/bootstrap.min.js')
-    .pipe(bootstrap())
     .pipe(gulp.dest('src/js'))
 });
 
-gulp.task('default', ['js']);
+gulp.task('estilo', function(){
+  return gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css')
+    .pipe(gulp.dest('src/css'))
+});
 
+gulp.task('jquery', function(){
+  return gulp.src('node_modules/jquery/dist/jquery.min.js')
+    .pipe(gulp.dest('src/js'))
+});
 
 //------- hasta aqui ----
 
@@ -36,4 +41,4 @@ gulp.task('images', function() {
 });
 
 
-gulp.task('default', ['images', 'style', 'script']);
+gulp.task('default', ['images', 'style', 'script','javascript','estilo', 'jquery']);
